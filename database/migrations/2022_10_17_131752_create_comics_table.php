30085dc219fc45588bcea5cb4fdd17dc0238cc08
masfreddy93 <masieronicola@gmail.com>
2022-10-17 15:53:12 +0200

    define tables

--- database/migrations/2022_10_17_131752_create_comics_table.php
+++ database/migrations/2022_10_17_131752_create_comics_table.php
@@ -15,6 +15,15 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title', 80);
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price', 6, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type', 30);
+
             $table->timestamps();
         });
     }
